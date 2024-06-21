3a43f871be2993b46731773fc1d63e66a4c2ea31
Barış ÖZ <mbarisoz97@gmail.com>
2024-06-21 22:29:08 +0300

    Add employee info database context

---
+++ src/Services/Ehrms.EmployeeInfo.API/Context/EmployeeInfoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ehrms.EmployeeInfo.API.Context;
+
+public class EmployeeInfoDbContext : DbContext
+{
+    public EmployeeInfoDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Skill> Skills { get; set; }
+}
--- src/Services/Ehrms.EmployeeInfo.API/Usings.cs
+++ src/Services/Ehrms.EmployeeInfo.API/Usings.cs
@@ -1,3 +1,5 @@
-﻿global using Ehrms.Shared.Common;
-global using Ehrms.EmployeeInfo.API.Dtos;
+﻿global using Microsoft.AspNetCore.Mvc;
+
+global using Ehrms.Shared.Common;
 global using Ehrms.EmployeeInfo.API.Models;
+global using Ehrms.EmployeeInfo.API.Dtos.Employee;
