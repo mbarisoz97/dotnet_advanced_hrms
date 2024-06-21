6e7e40dd30a4fcfc8b418e89b64ed63ed028b959
Barış ÖZ <mbarisoz97@gmail.com>
2024-06-21 22:11:26 +0300

    Add employee info entities

---
+++ src/Services/Ehrms.EmployeeInfo.API/Models/Skill.cs
@@ -0,0 +1,7 @@
+﻿namespace Ehrms.EmployeeInfo.API.Models;
+
+public sealed class Skill : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public ICollection<Employee> Employees { get; set; } = [];
+}
