ff2b2ea6c91809dd892274e160cd6c425eec4bdf
Barış ÖZ <mbarisoz97@gmail.com>
2024-06-24 14:29:09 +0200

    Configure training management api dependencies

---
+++ src/Services/Ehrms.TrainingManagement.API/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace Ehrms.TrainingManagement.API;
+
+internal static class DependencyInjection
+{
+	internal static IServiceCollection AddTrainingManagementApi(this IServiceCollection services)
+	{
+		AddThirdPartyLibraries(services);
+		return services;
+	}
+
+	private static IServiceCollection AddThirdPartyLibraries(IServiceCollection services)
+	{
+		var assembly = Assembly.GetExecutingAssembly();
+
+		services.AddAutoMapper(assembly);
+		services.AddMediatR(config =>
+		{
+			config.RegisterServicesFromAssembly(assembly);
+		});
+
+		return services;
+	}
+}
