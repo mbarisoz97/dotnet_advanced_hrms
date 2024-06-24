aea9d7b9ef4e93d63ea78577ed8b445697a55b08
Barış ÖZ <mbarisoz97@gmail.com>
2024-06-24 14:28:51 +0200

    Add training api controller

---
+++ src/Services/Ehrms.TrainingManagement.API/Controllers/TrainingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ehrms.TrainingManagement.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TrainingController : ControllerBase
+	{
+	}
+}
