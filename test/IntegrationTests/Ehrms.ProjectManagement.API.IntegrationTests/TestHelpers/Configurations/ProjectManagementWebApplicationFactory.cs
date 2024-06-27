e0abb76015ab77b8b8825171421e522bbacc9322
Barış ÖZ <mbarisoz97@gmail.com>
2024-06-27 09:59:38 +0300

    Use test containers and jwt token authentication for project management api integration tests

--- test/IntegrationTests/Ehrms.ProjectManagement.API.IntegrationTests/TestHelpers/Configurations/ProjectManagementWebApplicationFactory.cs
+++ test/IntegrationTests/Ehrms.ProjectManagement.API.IntegrationTests/TestHelpers/Configurations/ProjectManagementWebApplicationFactory.cs
@@ -1,18 +1,32 @@
-﻿using Ehrms.ProjectManagement.API.Context;
+﻿using DotNet.Testcontainers.Builders;
+using Ehrms.ProjectManagement.API.Context;
 using MassTransit;
-using MassTransit.Testing;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Testcontainers.MsSql;
 
 namespace Ehrms.ProjectManagement.API.IntegrationTests.TestHelpers.Configurations;
 
 internal class ProjectManagementWebApplicationFactory : WebApplicationFactory<Program>
 {
-    private readonly InMemoryTestHarness harness = new();
+    private int Port => Random.Shared.Next(1000, 60000);
+    private readonly MsSqlContainer _msSqlContainer;
+
+    public ProjectManagementWebApplicationFactory()
+    {
+        _msSqlContainer = new MsSqlBuilder()
+            .WithImage("mcr.microsoft.com/mssql/server")
+            .WithEnvironment("MSSQL_SA_PASSWORD", "yourStrong(!)Password")
+            .WithEnvironment("ACCEPT_EULA", "Y")
+            .WithPortBinding(Port, 1433)
+            .WithWaitStrategy(Wait.ForUnixContainer()
+                .UntilPortIsAvailable(1433))
+            .Build();
+    }
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
@@ -23,19 +37,38 @@ internal class ProjectManagementWebApplicationFactory : WebApplicationFactory<Pr
 
             services.AddDbContext<ProjectDbContext>(options =>
             {
-                options.UseInMemoryDatabase("ProjectManamagementDb");
+                options.UseSqlServer(_msSqlContainer.GetConnectionString(),
+                    opt => opt.EnableRetryOnFailure());
             });
 
-            var dbContext = CreateDbContext(services);
+            services.AddMassTransitTestHarness();
         });
     }
 
-    private ProjectDbContext CreateDbContext(IServiceCollection services)
+    private void EnsureDatabaseCreated()
     {
-        var serviceProvider = services.BuildServiceProvider();
-        var scope = serviceProvider.CreateScope();
+        var scope = Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetService<ProjectDbContext>();
+        dbContext!.Database.EnsureCreated();
+    }
 
-        return dbContext ?? throw new NullReferenceException("DbContext is null");
+    public async Task Start()
+    {
+        await _msSqlContainer.StartAsync();
+        
+        try
+        {
+            EnsureDatabaseCreated();
+        }
+        catch
+        {
+            await Stop();
+        }
+    }
+
+    public async Task Stop()
+    {
+        await _msSqlContainer.StopAsync();
+        await _msSqlContainer.DisposeAsync().AsTask();
     }
 }
---
+++ test/IntegrationTests/Ehrms.ProjectManagement.API.IntegrationTests/TestHelpers/Configurations/TestAuthHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace Ehrms.ProjectManagement.API.IntegrationTests.TestHelpers.Configurations;
+
+public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    public TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder)
+        : base(options, logger, encoder)
+    {
+    }
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var identity = new ClaimsIdentity(Array.Empty<Claim>(), "Test");
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, "TestScheme");
+
+        var result = AuthenticateResult.Success(ticket);
+
+        return Task.FromResult(result);
+    }
+}
