b938c72bc35b5cccabf52f9092b719f87489c4ef
Barış ÖZ <mbarisoz97@gmail.com>
2024-07-01 16:38:45 +0200

    Add payroll api controller

---
+++ src/Services/Ehrms.Administration.API/Controllers/PayrollController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ehrms.Administration.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PayrollController : ControllerBase
+{
+}
