8bb465069ef99a683418fe20ae93d90e9f99bf9e
Barış ÖZ <mbarisoz97@gmail.com>
2024-07-11 23:11:54 +0200

    Add skill index page

--- src/FrontEnd/Ehrms.Web/Ehrms.Web/Client/IEmployeeInfoServiceClient.cs
+++ src/FrontEnd/Ehrms.Web/Ehrms.Web/Client/IEmployeeInfoServiceClient.cs
@@ -1,4 +1,8 @@
-﻿using Ehrms.Web.Models;
+﻿using BitzArt.Blazor.Cookies;
+using Ehrms.Web.Models;
+using Ehrms.Web.Routing;
+using System.Net;
+using System.Net.Http.Headers;
 
 namespace Ehrms.Web.Client;
 
@@ -9,4 +13,80 @@ public interface IEmployeeServiceClient
 	Task DeleteEmployeeAsync(Guid id);
 	Task<Employee> GetEmployeeAsync(Guid id);
 	Task<IEnumerable<Employee>> GetEmployeesAsync(int pageNumber, int pageSize = 20);
+}
+
+public sealed class Response<T>
+{
+	public HttpStatusCode StatusCode { get; set; }
+	public T? Content { get; set; }
+}
+
+public interface ISkillServiceClient
+{
+	Task<Response<IEnumerable<SkillModel>>> GetSkillsAsync();
+}
+
+public interface IHttpClientFactoryWrapper
+{
+	Task<HttpClient> CreateClient(string name);
+}
+
+internal sealed class HttpClientFactoryWrapper : IHttpClientFactoryWrapper
+{
+	private readonly ICookieService _cookieService;
+	private readonly IHttpClientFactory _httpClientFactory;
+
+	public HttpClientFactoryWrapper(IHttpClientFactory httpClientFactory, ICookieService cookieService)
+	{
+		_cookieService = cookieService;
+		_httpClientFactory = httpClientFactory;
+	}
+
+	public async Task<HttpClient> CreateClient(string name)
+	{
+		var client = _httpClientFactory.CreateClient(name);
+		var accessToken = await _cookieService.GetAsync(CookieKeys.AccessToken);
+		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken?.Value);
+
+		return client;
+	}
+}
+
+internal sealed class SkillServiceClient : ISkillServiceClient
+{
+	private readonly IEndpointProvider _endpointProvider;
+	private readonly IHttpClientFactoryWrapper _clientFactoryWrapper;
+
+	public SkillServiceClient(IHttpClientFactoryWrapper clientFactoryWrapper, IEndpointProvider endpointProvider)
+	{
+		_clientFactoryWrapper = clientFactoryWrapper;
+		_endpointProvider = endpointProvider;
+	}
+
+	public async Task<Response<IEnumerable<SkillModel>>> GetSkillsAsync()
+	{
+		var client = await _clientFactoryWrapper.CreateClient("ApiGateway");
+		var response = await client.GetAsync(_endpointProvider.EmployeeInfoServiceEndpoint);
+
+		return new Response<IEnumerable<SkillModel>>()
+		{
+			StatusCode = response.StatusCode,
+			Content = await response.GetContentAs<IEnumerable<SkillModel>>()
+		};
+	}
+}
+
+internal static class HttpResponseExtensions
+{
+	internal static async Task<T?> GetContentAs<T>(this HttpResponseMessage httpResponseMessage)
+	{
+		try
+		{
+			return await httpResponseMessage.Content.ReadFromJsonAsync<T>();
+		}
+		catch
+		{
+			return default;
+		}
+	}
 }
--- src/FrontEnd/Ehrms.Web/Ehrms.Web/Routing/EmployeeRouting.cs
+++ src/FrontEnd/Ehrms.Web/Ehrms.Web/Routing/EmployeeRouting.cs
@@ -7,3 +7,11 @@ internal static class EmployeeRouting
     internal const string Update = $"{Index}/Update";
     internal const string Delete = $"{Index}/Delete";
 }
+
+internal static class SkillRouting
+{
+	internal const string Index = "/Skill";
+	internal const string Create = $"{Index}/Create";
+	internal const string Update = $"{Index}/Update";
+	internal const string Delete = $"{Index}/Delete";
+}
