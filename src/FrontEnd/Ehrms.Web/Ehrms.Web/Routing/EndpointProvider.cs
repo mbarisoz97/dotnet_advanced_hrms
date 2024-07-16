94814dce3811ed455267c23e4dd578cc01a92470
Barış ÖZ <mbarisoz97@gmail.com>
2024-07-16 22:30:05 +0200

    Use corrent endpoint for project client service

--- src/FrontEnd/Ehrms.Web/Ehrms.Web/Routing/EndpointProvider.cs
+++ src/FrontEnd/Ehrms.Web/Ehrms.Web/Routing/EndpointProvider.cs
@@ -5,7 +5,7 @@ internal sealed class EndpointProvider : IEndpointProvider
 	public string RefreshEndpoint => "/api/Account/Refresh";
 	public string AutheticationEndpoint => "/api/Account/Login";
 	public string EmployeeInfoServiceEndpoint => "/api/Skill";
-	public string? ProjectEndpoint => "/api/Project";
+	public string? ProjectManagementApiEndpoint => "/api/Project";
 	public string? EmploymentApiEndpoint => "/api/Project/Employment";
 	public string? TrainingManagementServiceEndpoint => "/api/Training";
 }
