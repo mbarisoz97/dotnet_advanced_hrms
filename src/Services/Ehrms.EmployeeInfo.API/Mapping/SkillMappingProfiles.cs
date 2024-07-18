a90cf422caf3b242fe203f1f36aebf34e68c040e
Barış ÖZ <mbarisoz97@gmail.com>
2024-07-18 09:56:52 +0200

    Publish skill created event after successfull create command

--- src/Services/Ehrms.EmployeeInfo.API/Mapping/SkillMappingProfiles.cs
+++ src/Services/Ehrms.EmployeeInfo.API/Mapping/SkillMappingProfiles.cs
@@ -1,16 +1,23 @@
-﻿using Ehrms.EmployeeInfo.API.Database.Models;
+﻿using Ehrms.Contracts.Skill;
+using Ehrms.EmployeeInfo.API.Database.Models;
 
 namespace Ehrms.EmployeeInfo.API.Mapping;
 
 public class SkillMappingProfiles : Profile
 {
     public SkillMappingProfiles()
-    {
-        AddEntityToDtoMappings();
-        AddCommandToEntityMappings();
-    }
+	{
+		AddEntityToDtoMappings();
+		AddCommandToEntityMappings();
+		AddEntityToMessageQueueEventMappings();
+	}
 
-    private void AddCommandToEntityMappings()
+	private void AddEntityToMessageQueueEventMappings()
+	{
+		CreateMap<Skill, SkillCreatedEvent>();
+	}
+
+	private void AddCommandToEntityMappings()
     {
         CreateMap<UpdateSkillCommand, Skill>();
         CreateMap<CreateSkillCommand, Skill>();
---
+++ test/UnitTests/Ehrms.EmployeeInfo.API.UnitTests/TestHelpers/Fakers/Command/CreateSkillCommandFaker.cs
@@ -0,0 +1,17 @@
+﻿using Ehrms.EmployeeInfo.API.Handlers.Skill.Command;
+
+namespace Ehrms.EmployeeInfo.API.UnitTests.TestHelpers.Fakers.Command;
+
+internal class CreateSkillCommandFaker : Faker<CreateSkillCommand>
+{
+	public CreateSkillCommandFaker()
+	{
+		RuleFor(x => x.Name, f => f.Name.Random.Words(3));
+	}
+
+	public CreateSkillCommandFaker WithName(string name)
+	{
+		RuleFor(x => x.Name, name);
+		return this;
+	}
+}
