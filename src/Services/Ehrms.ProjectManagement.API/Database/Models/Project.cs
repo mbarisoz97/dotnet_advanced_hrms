80dd9ba0a5593649ac82e50f076c8cb3fd5be6ce
Barış ÖZ <mbarisoz97@gmail.com>
2024-07-18 10:57:34 +0200

    Update project management database configuration models

--- src/Services/Ehrms.ProjectManagement.API/Database/Models/Project.cs
+++ src/Services/Ehrms.ProjectManagement.API/Database/Models/Project.cs
@@ -4,5 +4,7 @@ public class Project : BaseEntity
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public ICollection<Employment> Employments { get; set; } = [];
+	
+	public ICollection<Employment> Employments { get; set; } = [];
+	public ICollection<Skill> RequiredProjectSkills { get; set; } = [];
 }
