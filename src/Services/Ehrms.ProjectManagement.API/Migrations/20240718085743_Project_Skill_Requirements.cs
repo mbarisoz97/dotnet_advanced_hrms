f1830fe6ede07065f71adb3112d32f64eb4f2fe1
Barış ÖZ <mbarisoz97@gmail.com>
2024-07-18 10:58:09 +0200

    Add project skill requirement migration for project management database

---
+++ src/Services/Ehrms.ProjectManagement.API/Migrations/20240718085743_Project_Skill_Requirements.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ehrms.ProjectManagement.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Project_Skill_Requirements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectSkill",
+                columns: table => new
+                {
+                    ProjectsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RequiredProjectSkillsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectSkill", x => new { x.ProjectsId, x.RequiredProjectSkillsId });
+                    table.ForeignKey(
+                        name: "FK_ProjectSkill_Projects_ProjectsId",
+                        column: x => x.ProjectsId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProjectSkill_Skills_RequiredProjectSkillsId",
+                        column: x => x.RequiredProjectSkillsId,
+                        principalTable: "Skills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectSkill_RequiredProjectSkillsId",
+                table: "ProjectSkill",
+                column: "RequiredProjectSkillsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjectSkill");
+
+            migrationBuilder.DropTable(
+                name: "Skills");
+        }
+    }
+}
