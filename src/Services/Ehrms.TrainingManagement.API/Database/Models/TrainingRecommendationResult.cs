5700059d7bbf8857352b244797f491c99cf0af9c
Barış ÖZ <mbarisoz97@gmail.com>
2024-08-07 23:25:52 +0300

    Delete recommendation requests and results

--- src/Services/Ehrms.TrainingManagement.API/Database/Models/TrainingRecommendationResult.cs
+++ src/Services/Ehrms.TrainingManagement.API/Database/Models/TrainingRecommendationResult.cs
@@ -3,6 +3,6 @@
 public sealed class TrainingRecommendationResult : BaseEntity
 {
 	public Skill? Skill { get; set; }
-    public TrainingRecommendationRequest? RecommendationRequest { get; set; }
     public ICollection<Employee> Employees { get; set; } = [];
+    public TrainingRecommendationRequest? RecommendationRequest { get; set; }
 }
--- src/Services/Ehrms.TrainingManagement.API/Handlers/Training/Commands/DeleteTrainingRecommendationRequestCommand.cs
+++ src/Services/Ehrms.TrainingManagement.API/Handlers/Training/Commands/DeleteTrainingRecommendationRequestCommand.cs
@@ -19,7 +19,13 @@ internal sealed class DeleteTrainingRecommendationRequestCommandHandler : IReque
         var recommendationRequest = await _dbContext.RecommendationRequests.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken) 
                                     ?? throw new TrainingRecommendationRequestNotFoundException($"Could not find recommendation request with id : <{request.Id}>");
 
+        var recommendationResults =  _dbContext.RecommendationResults
+            .Include(x => x.RecommendationRequest)
+            .Where(x => x.RecommendationRequest!.Id == request.Id);
+        
+        _dbContext.RemoveRange(recommendationResults);
         _dbContext.Remove(recommendationRequest);
+
         await _dbContext.SaveChangesAsync(cancellationToken);
     }
 }
