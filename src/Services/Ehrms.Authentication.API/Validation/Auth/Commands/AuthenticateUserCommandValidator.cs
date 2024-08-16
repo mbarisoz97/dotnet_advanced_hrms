09640ac362fda404329254acd309989c049b3be6
Barış ÖZ <mbarisoz97@gmail.com>
2024-08-16 15:38:25 +0200

    Add authentication command validators

---
+++ src/Services/Ehrms.Authentication.API/Validation/Auth/Commands/AuthenticateUserCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using Ehrms.Authentication.API.Handlers.Auth.Commands;
+
+namespace Ehrms.Authentication.API.Validation.Auth.Commands;
+
+public class AuthenticateUserCommandValidator : AbstractValidator<AuthenticateUserCommand>
+{
+    public AuthenticateUserCommandValidator()
+    {
+        RuleFor(x => x.Username)
+            .NotEmpty();
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+    }
+}
