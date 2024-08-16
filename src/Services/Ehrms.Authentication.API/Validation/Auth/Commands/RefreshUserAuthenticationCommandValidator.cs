3ac882b26934e37c486b54a51d252ca5e3fb31f8
Barış ÖZ <mbarisoz97@gmail.com>
2024-08-16 15:38:25 +0200

    Add authentication command validators

---
+++ src/Services/Ehrms.Authentication.API/Validation/Auth/Commands/RefreshUserAuthenticationCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using Ehrms.Authentication.API.Handlers.Auth.Commands;
+
+namespace Ehrms.Authentication.API.Validation.Auth.Commands;
+
+public class RefreshUserAuthenticationCommandValidator : AbstractValidator<RefreshAuthenticationCommand>
+{
+    public RefreshUserAuthenticationCommandValidator()
+    {
+        RuleFor(x => x.RefreshToken)
+            .NotEmpty();
+
+        RuleFor(x=>x.AccessToken)
+            .NotEmpty();
+    }
+}
