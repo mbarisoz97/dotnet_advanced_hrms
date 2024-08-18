ca32522d4e6a77b25ae1cd6a2e08f3dc0af3854b
Barış ÖZ <mbarisoz97@gmail.com>
2024-08-18 17:42:42 +0300

    Remove redundant mock user manager methods

--- test/UnitTests/Ehrms.Authentication.API.UnitTests/TestHelpers/Mock/MockUserManager.cs
+++ test/UnitTests/Ehrms.Authentication.API.UnitTests/TestHelpers/Mock/MockUserManager.cs
@@ -3,6 +3,7 @@ using Ehrms.Authentication.API.Database.Context;
 using Ehrms.Authentication.TestHelpers.Faker.Models;
 
 namespace Ehrms.Authentication.API.UnitTests.TestHelpers.Mock;
+
 public sealed class MockUserManager : Mock<IUserManagerAdapter>
 {
     private readonly ApplicationUserDbContext _dbContext;
@@ -28,80 +29,5 @@ public sealed class MockUserManager : Mock<IUserManagerAdapter>
     {
         Setup(x => x.UpdateAsync(It.IsAny<User>()))
             .ReturnsAsync(identityResult);
-    }
-
-    public void SetupRemoveFromRolesAsync(IdentityResult identityResult)
-    {
-        Setup(x => x.RemoveFromRolesAsync(It.IsAny<User>(), It.IsAny<IEnumerable<string>>()))
-              .Callback(async (User user, IEnumerable<string> roleNameCollection) =>
-              {
-                  await MockRemoveRolesAsync(user, roleNameCollection);
-              })
-             .ReturnsAsync(identityResult);
-    }
-    private async Task MockRemoveRolesAsync(User user, IEnumerable<string> roleNameCollection)
-    {
-        foreach (var roleName in roleNameCollection)
-        {
-            var role = _dbContext.Roles!.FirstOrDefault(r => r.Name == roleName);
-            if (role == null)
-            {
-                continue;
-            }
-
-            var userRole = _dbContext.UserRoles.FirstOrDefault(x => x.UserId == user.Id && x.RoleId == role.Id);
-            if (userRole != null)
-            {
-                user.UserRoles.Remove(userRole);
-            }
-        }
-        await _dbContext.SaveChangesAsync();
-    }
-
-    private async Task MockRemoveRolesAsync(User user, IEnumerable<string> roleNameCollection)
-    {
-        foreach (var roleName in roleNameCollection)
-        {
-            var role = _dbContext.Roles!.FirstOrDefault(r => r.Name == roleName);
-            if (role == null)
-            {
-                continue;
-            }
-
-            var userRole = _dbContext.UserRoles.FirstOrDefault(x => x.UserId == user.Id && x.RoleId == role.Id);
-            if (userRole != null)
-            {
-                user.UserRoles.Remove(userRole);
-            }
-        }
-        await _dbContext.SaveChangesAsync();
-    }
-
-    public void SetupAddToRolesAsync(IdentityResult identityResult)
-    {
-        Setup(x => x.AddToRolesAsync(It.IsAny<User>(), It.IsAny<IEnumerable<string>>()))
-            .Callback(async (User user, IEnumerable<string> roleNameCollection) =>
-            {
-                await MockAddRolesToUser(user, roleNameCollection);
-            })
-            .ReturnsAsync(IdentityResult.Success);
-    }
-    private async Task MockAddRolesToUser(User user, IEnumerable<string> roleNameCollection)
-    {
-        foreach (var roleName in roleNameCollection)
-        {
-            var role = new RoleFaker()
-                .WithName(roleName)
-                .Generate();
-            await _dbContext.AddAsync(role);
-
-            var userRole = new UserRoleFaker()
-                .WithUser(user)
-                .WithRole(role)
-                .Generate();
-
-            await _dbContext.AddAsync(userRole);
-        }
-        await _dbContext.SaveChangesAsync();
     }
 }
