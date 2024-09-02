104932a104662212e9f4066093a5f456fc47e6b7
Barış ÖZ <mbarisoz97@gmail.com>
2024-09-02 14:33:54 +0200

    Add employee info api transaction behavior

---
+++ src/Services/Ehrms.EmployeeInfo.API/PipelineBehaviors/TransactionBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Ehrms.EmployeeInfo.API.Database.Context;
+
+namespace Ehrms.EmployeeInfo.API.Behaviour;
+
+public class TransactionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+	private readonly EmployeeInfoDbContext _dbContext;
+
+    public TransactionBehavior(EmployeeInfoDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        await using var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
+        try
+        {
+            var result = await next();
+            await transaction.CommitAsync(cancellationToken);
+            return result;
+        }
+        catch
+        {
+            await transaction.RollbackAsync(cancellationToken);
+            throw;
+        }
+    }
+}
