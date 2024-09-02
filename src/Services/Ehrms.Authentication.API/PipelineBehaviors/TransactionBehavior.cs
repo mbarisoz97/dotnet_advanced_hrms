6f75d88d1dfd45fa01ed1b75cba7bae7e57e4dd1
Barış ÖZ <mbarisoz97@gmail.com>
2024-09-02 14:32:38 +0200

    Add authentication api transaction behavior

---
+++ src/Services/Ehrms.Authentication.API/PipelineBehaviors/TransactionBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Ehrms.Authentication.API.Database.Context;
+
+namespace Ehrms.Authentication.API.PipelineBehaviors;
+
+public class TransactionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> 
+    where TRequest : IRequest<TResponse>
+{
+    private readonly ApplicationUserDbContext _dbContext;
+
+    public TransactionBehavior(ApplicationUserDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        await using var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
+        try
+        {
+            var result = await next();
+            await transaction.CommitAsync(cancellationToken);   
+            return result;
+        }
+        catch
+        {
+            await transaction.RollbackAsync(cancellationToken);
+            throw;
+        }
+    }
+}
