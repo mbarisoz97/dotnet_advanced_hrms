b560cca79a604962cf481cab1010ff1289d85d1c
Barış ÖZ <mbarisoz97@gmail.com>
2024-09-06 10:42:19 +0200

    Add validation attributes for login request model

--- src/FrontEnd/Ehrms.Web/Ehrms.Web/Models/LoginRequestModel.cs
+++ src/FrontEnd/Ehrms.Web/Ehrms.Web/Models/LoginRequestModel.cs
@@ -1,7 +1,14 @@
-﻿namespace Ehrms.Web.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ehrms.Web.Models;
 
 public class LoginRequestModel
 {
-	public string Username { get; set; }
-	public string Password { get; set; }
+	[Required]
+	public string Username { get; set; }= string.Empty;
+	
+	[Required]
+    [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
+    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$^+=!*()@%&]).{8,}$", ErrorMessage = "Password should have at least one lower case, one upper case, one number, one special character")]
+    public string Password { get; set; } = string.Empty;
 }
