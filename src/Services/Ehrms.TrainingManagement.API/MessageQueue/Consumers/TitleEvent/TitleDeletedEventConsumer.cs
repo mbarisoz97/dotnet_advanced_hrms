48b5fa915d0dc78ecabbab51e19cd47c6b8485cf
Barış ÖZ <mbarisoz97@gmail.com>
2024-09-20 19:24:29 +0300

    Add title deleted event consumer for training management api

---
+++ src/Services/Ehrms.TrainingManagement.API/MessageQueue/Consumers/TitleEvent/TitleDeletedEventConsumer.cs
@@ -0,0 +1,28 @@
+﻿using Ehrms.Contracts.Title;
+
+namespace Ehrms.TrainingManagement.API.MessageQueue.Consumers.TitleEvent;
+
+public class TitleDeletedEventConsumer : IConsumer<TitleDeletedEvent>
+{
+    private readonly ILogger<TitleDeletedEventConsumer> _logger;
+    private readonly TrainingDbContext _dbContext;
+
+    public TitleDeletedEventConsumer(TrainingDbContext dbContext, ILogger<TitleDeletedEventConsumer> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    public async Task Consume(ConsumeContext<TitleDeletedEvent> context)
+    {
+        var existingTitle = _dbContext.Titles.FirstOrDefault(x=>x.Id == context.Message.Id);
+        if (existingTitle == null)
+        {
+            _logger.LogError("Ignored title deleted event. Could not find title with id : <{id}>", context.Message.Id);
+            return;
+        }
+
+        _dbContext.Remove(existingTitle);
+        await _dbContext.SaveChangesAsync();
+    }
+}
---
+++ test/UnitTests/Ehrms.TrainingManagement.API.UnitTests/Consumers/Title/TitleDeletedEventConsumerTests.cs
@@ -0,0 +1,52 @@
+﻿using Ehrms.Contracts.Title;
+using Ehrms.TrainingManagement.API.MessageQueue.Consumers.TitleEvent;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Ehrms.TrainingManagement.API.UnitTests.Consumers.Title;
+
+public class TitleDeletedEventConsumerTests
+{
+    private readonly TrainingDbContext _dbContext;
+    private readonly TitleDeletedEventConsumer _consumer;
+    private readonly Mock<ILogger<TitleDeletedEventConsumer>> _loggerMock = new();
+
+    public TitleDeletedEventConsumerTests()
+    {
+        _dbContext = TestDbContextFactory.CreateDbContext(Guid.NewGuid().ToString());
+        _consumer = new TitleDeletedEventConsumer(_dbContext, _loggerMock.Object);
+    }
+
+    [Fact]
+    public async Task Consume_NonExistingTitleId_IgnoresEvent()
+    {
+        var titleDeletedEvent = new TitleDeletedEventFaker().Generate();
+
+        Mock<ConsumeContext<TitleDeletedEvent>> contextMock = new();
+        contextMock.Setup(x => x.Message).Returns(titleDeletedEvent);
+
+        await _consumer.Consume(contextMock.Object);
+
+        _dbContext.Titles.Count().Should().Be(0);   
+    }
+
+    [Fact]
+    public async Task Consume_ExistingTitleId_RemovesTitlet()
+    {
+        var title = new TitleFaker().Generate();
+        await _dbContext.AddAsync(title);
+        await _dbContext.SaveChangesAsync();
+
+        var titleDeletedEvent = new TitleDeletedEventFaker()
+            .WithId(title.Id)
+            .Generate();
+
+        Mock<ConsumeContext<TitleDeletedEvent>> contextMock = new();
+        contextMock.Setup(x => x.Message).Returns(titleDeletedEvent);
+
+        await _consumer.Consume(contextMock.Object);
+
+        _dbContext.Titles.Count().Should().Be(0);
+    }
+}
