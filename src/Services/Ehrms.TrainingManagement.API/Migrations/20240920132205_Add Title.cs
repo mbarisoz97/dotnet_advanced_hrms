77ae093c6f08d880f956116b5fe93f47d04ff73f
Barış ÖZ <mbarisoz97@gmail.com>
2024-09-20 15:28:18 +0200

    Add title migration for training management db

---
+++ src/Services/Ehrms.TrainingManagement.API/Migrations/20240920132205_Add Title.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ehrms.TrainingManagement.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Titles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Titles", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Titles");
+        }
+    }
+}
