d299975e39bfd3b4f8e22d53b5f182681c5ba35b
Barış ÖZ <mbarisoz97@gmail.com>
2024-10-10 09:59:49 +0200

    Use shared http client name for payment category client

---
+++ src/FrontEnd/Ehrms.Web/Ehrms.Web/Client/Administration/PaymentCategoryClient.cs
@@ -0,0 +1,75 @@
+﻿using Ehrms.Web.Models.Administration;
+
+namespace Ehrms.Web.Client.Administration;
+
+public class PaymentCategoryClient : IPaymentCategoryClient
+{
+    private const string PaymentCategoryEndpoint = "/api/PaymentCategory";
+
+    private readonly IHttpClientFactoryWrapper _httpClientFactoryWrapper;
+
+    public PaymentCategoryClient(IHttpClientFactoryWrapper httpClientFactoryWrapper)
+    {
+        _httpClientFactoryWrapper = httpClientFactoryWrapper;
+    }
+
+    public async Task<Response<PaymentCategoryModel>> CreatePaymentCategory(PaymentCategoryModel paymentCategory)
+    {
+        var client = await _httpClientFactoryWrapper.CreateClient(HttpClients.BackendApiGateway);
+        var response = await client.PutAsJsonAsync(PaymentCategoryEndpoint, paymentCategory);
+
+        return new Response<PaymentCategoryModel>
+        {
+            StatusCode = response.StatusCode,
+            Content = await response.GetContentAs<PaymentCategoryModel>()
+        };
+    }
+
+    public async Task<Response<Guid>> DeletePaymentRecordById(Guid id)
+    {
+        var client = await _httpClientFactoryWrapper.CreateClient(HttpClients.BackendApiGateway);
+        var response = await client.DeleteAsync($"{PaymentCategoryEndpoint}/{id}");
+
+        return new Response<Guid>
+        {
+            StatusCode = response.StatusCode,
+            Content = await response.GetContentAs<Guid>()
+        };
+    }
+
+    public async Task<Response<IEnumerable<PaymentCategoryModel>>> GetPaymentCategories()
+    {
+        var client = await _httpClientFactoryWrapper.CreateClient(HttpClients.BackendApiGateway);
+        var response = await client.GetAsync(PaymentCategoryEndpoint);
+
+        return new Response<IEnumerable<PaymentCategoryModel>>
+        {
+            StatusCode = response.StatusCode,
+            Content = await response.GetContentAs<IEnumerable<PaymentCategoryModel>>()
+        };
+    }
+
+    public async Task<Response<PaymentCategoryModel>> GetPaymentCategoryById(Guid id)
+    {
+        var client = await _httpClientFactoryWrapper.CreateClient(HttpClients.BackendApiGateway);
+        var response = await client.GetAsync($"{PaymentCategoryEndpoint}/{id}");
+
+        return new Response<PaymentCategoryModel>
+        {
+            StatusCode = response.StatusCode,
+            Content = await response.GetContentAs<PaymentCategoryModel>()
+        };
+    }
+
+    public async Task<Response<PaymentCategoryModel>> UpdatePaymentCategory(PaymentCategoryModel paymentCategory)
+    {
+        var client = await _httpClientFactoryWrapper.CreateClient(HttpClients.BackendApiGateway);
+        var response = await client.PostAsJsonAsync(PaymentCategoryEndpoint, paymentCategory);
+
+        return new Response<PaymentCategoryModel>
+        {
+            StatusCode = response.StatusCode,
+            Content = await response.GetContentAs<PaymentCategoryModel>()
+        };
+    }
+}
