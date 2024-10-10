2d04f0e790fc572d6c458e44531a40e99b26557b
Barış ÖZ <mbarisoz97@gmail.com>
2024-10-10 10:00:01 +0200

    Refactor payment category index page

---
+++ src/FrontEnd/Ehrms.Web/Ehrms.Web/Extensions/DialogResultExtensions.cs
@@ -0,0 +1,11 @@
+﻿using MudBlazor;
+
+namespace Ehrms.Web.Extensions;
+
+internal static class DialogResultExtensions
+{
+    internal static bool IsOk(this DialogResult? dialogResult)
+    {
+        return dialogResult != null && !dialogResult.Canceled;
+    }
+}
